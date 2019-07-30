587fc7fc3f70b39a669ac27db1c1a2a3073923c9
ThunderSmotch <2165764+ThunderSmotch@users.noreply.github.com>
2019-07-30 04:45:18 +0100

    Style and Boilerplate cuts
    First style choices and layout started

--- js/main.js
+++ js/main.js
@@ -1,1 +1,38 @@
+//Change Theme
+function changeTheme(value) {
+  switch (value) {
+    case 1:
+      $("#ref-css").attr("href", "css/bootstrap.min.css");
+      console.log(1);
+      break;
+    case 2:
+      $("#ref-css").attr("href", "css/flatly.min.css");
+      break;
+    case 3:
+      $("#ref-css").attr("href", "css/darkly.min.css");
+      break;
+    default:
+      //$("#ref-css").attr("href", "css/bootstrap.min.css");
+  }
+  console.log("Theme Changed - " + value);
+  localStorage.setItem("theme", value);
+}
 
+//Listener Theme Changing
+$(function() {
+  $('#selectTheme').change(function() {
+    var value = parseInt($(this).val());
+    changeTheme(value);
+  })
+})
+
+//On Document Ready
+$(window).on('load', function() {
+  console.log("Grabbing saved theme.");
+  if (localStorage.getItem("theme"))
+    changeTheme(parseInt(localStorage.getItem("theme")));
+
+  setTimeout(function() {
+    $("#cover").hide();
+  }, 500);
+});
